72134c7c122ac920183b012837dfcd8d0233a95c
netiapaul <netiapaul23@gmail.com>
2021-03-16 13:34:17 +0300

    added correct species labeling

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,214 +1,182 @@
-import React,{useState,useEffect} from 'react';
-import { useHistory } from 'react-router';
-import Header from '../header/header';
-import './home.css';
-import { Formik, Form, Field, FieldArray } from 'formik';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import Header from "../header/header";
+import "./home.css";
+import { Formik, Form, Field, FieldArray } from "formik";
 
-// const initialValues = Object.freeze({
-//     name: "",
-//     longitude: "",
-//     latitude: "",
-//     species:[]
-//   });
-  
-  
-const Home = () => { 
-    let history = useHistory();
+const Home = () => {
+  let history = useHistory();
 
-    const [data,setData] = useState("")
-    
-   
-    useEffect(()=>{
-      let user = JSON.parse(localStorage.getItem('user'));
-      setData(user)
-        
-    },[data]);
+  const [token, setToken] = useState("");
 
-    // const [values, setValues] = useState(initialValues);
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem("token"));
+    setToken(user);
+  }, [token]);
 
-    // const handleInputChange = (e) => {
+  const logOut = () => {
+    localStorage.clear();
+    history.push("/");
+  };
 
-    //     const { name, value } = e.target;
-    
-    //     setValues({
-    //       ...values,
-    //       [name]: value,
-    //     });
-    // };
+  return (
+    <>
+      {/* Image and text  */}
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log(values);
-    //     console.log(data);
+      <Header />
 
+      <div className="card mt-5 mx-auto">
+        <div className="card-body">
+          <Formik
+            initialValues={{
+              name: "",
+              longitude: "",
+              latitude: "",
+              species: [{ name: "" }],
+            }}
+            onSubmit={
+              (values) =>
+                // fetch('https://jsonplaceholder.typicode.com/posts')
+                // fetch('https://sitematching.herokuapp.com/api/v1/speciesToSite/', {
+                //     method: 'POST',
+                //     body: JSON.stringify(values),
+                //     headers: {
+                //     "Content-type": "application/json; charset=UTF-8"
+                //     }
+                // })
 
-    // }
-
-    const logOut = () =>{
-        localStorage.clear();
-        history.push('/')
-    }
-
-    return(
-        <>
-            {/* Image and text  */}
-    
-            <Header />
-            
-            {/* Form Card */}
-            {/* <div className="card mt-5 mx-auto">
-
-                <div className="card-body">
-
-                <form onSubmit={handleSubmit}>
-
-                    <div className="mb-3 has-validation">
-
-                        <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" name="name" value={values.name} onChange={handleInputChange} placeholder="e.g Mwihoko" required/>
-
-                    </div>
-
-                    <div className="mb-3">
-
-                        <label htmlFor="longitude" className="form-label">Longitude</label>
-                        <input type="number" className="form-control" id="longitude" name="longitude" value={values.longitude} onChange={handleInputChange} placeholder="e.g 36.6588983433" required/>
-
-
-                    </div>
-
-                    <div className="mb-3">
-
-                        <label htmlFor="latitude" className="form-label">Latitude</label>
-                        <input type="number" className="form-control" id="latitude" name="latitude" value={values.latitude} onChange={handleInputChange} placeholder="e.g -1.20266883724" required/>
-
-
-                    </div>
-
-                    <div className="mb-3">
-
-                        <label htmlFor="species" className="form-label">Species</label>
-                        <input type="text" className="form-control" id="species" name="species" value={values.species} onChange={handleInputChange} placeholder="e.g neema" required/>
-
-
-                    </div>
-
-                    <div className="d-flex justify-content-between">
-                    <button type="submit" className={data ? "btn btn-primary":"btn btn-primary disabled"}>Submit Data</button>
-                    <button type="submit" className="btn btn-outline-danger" onClick={logOut}>Logout</button>
-                    </div>
-
-                    
-                    
-                </form>
-                
+                fetch(
+                  "https://sitematching.herokuapp.com/api/v1/speciesToSite/",
+                  {
+                    method: "POST",
+                    headers: new Headers({
+                      Authorization: `Bearer ${token}`,
+                      Accept: "application/json",
+                      "Content-Type": "application/json",
+                    }),
+                    body: JSON.stringify(values),
+                  }
+                )
+                  .then((response) => response.json())
+                  .then((json) => console.log(json))
+                  .catch((err) => console.log(err))
+              // console.log(JSON.stringify(values, null, 2))
+            }
+            render={({ values }) => (
+              <Form>
+                <div className="mb-3">
+                  <label htmlFor="name">Name</label>
+                  <Field
+                    className="form-control"
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="e.g Mwihoko"
+                    required
+                  />
                 </div>
 
-            </div> */}
+                <div className="mb-3">
+                  <label htmlFor="longitude" className="form-label">
+                    Longitude
+                  </label>
+                  <Field
+                    className="form-control"
+                    id="name"
+                    type="number"
+                    id="longitude"
+                    name="longitude"
+                    placeholder="e.g 36.6588983433"
+                    required
+                  />
+                </div>
 
-        <div className="card mt-5 mx-auto">
+                <div className="mb-3">
+                  <label htmlFor="latitude" className="form-label">
+                    Latitude
+                  </label>
+                  <Field
+                    className="form-control"
+                    id="name"
+                    type="number"
+                    id="latitude"
+                    name="latitude"
+                    placeholder="e.g -1.20266883724"
+                    required
+                  />
+                </div>
 
-        <div className="card-body">
-
-        <Formik initialValues={{ name:"",longitude:"",latitude:"",species: [] }}
-
-                onSubmit={values =>
-                    // fetch('https://jsonplaceholder.typicode.com/posts')
-                    // fetch('https://sitematching.herokuapp.com/api/v1/speciesToSite/', {
-                    //     method: 'POST',
-                    //     body: JSON.stringify(values),
-                    //     headers: {
-                    //     "Content-type": "application/json; charset=UTF-8"
-                    //     }
-                    // })
-                    
-                    fetch('https://sitematching.herokuapp.com/api/v1/speciesToSite/', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                      },
-                    body: JSON.stringify(values)
-                })
-                .then(response => response.json())
-                .then(json => console.log(json))
-                    // console.log(JSON.stringify(values, null, 2))
-                }
-
-                render={({ values }) => (
-                    <Form>
-
-                    <div className="mb-3">
-                        <label htmlFor="name">Name</label>
-                        <Field className="form-control" id="name" type="text" name="name" placeholder="e.g Mwihoko" required />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="longitude" className="form-label">Longitude</label>
-                        <Field className="form-control" id="name" type="number" id="longitude" name="longitude" placeholder="e.g 36.6588983433" required />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="latitude" className="form-label">Latitude</label>
-                        <Field className="form-control" id="name" type="number" id="latitude" name="latitude" placeholder="e.g -1.20266883724" required />
-                    </div>
-                    
-                    <FieldArray
-                        name="species"
-                        render={arrayHelpers => (
-                        <div>
-                            {values.species && values.species.length > 0 ? (
-                            values.species.map((species, index) => (
-                                <div key={index} className="input-group mb-3">
-                                <Field name={`species.${index}`} className="form-control" placeholder="e.g neema" required />
-                                <button
-                                    className="btn btn-outline-danger"
-                                    type="button"
-                                    onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                                >
-                                    -
-                                </button>
-                                <button
-                                    className="btn btn-outline-success"
-                                    type="button"
-                                    onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                                >
-                                    +
-                                </button>
-                                </div>
-                            ))
-                            ) : (
-                            <button  className="btn btn-outline-secondary mb-3" type="button" onClick={() => arrayHelpers.push('')}>
-                                {/* show this when user has removed all friends from the list */}
-                                Add a species
+                <FieldArray
+                  name="species"
+                  render={(arrayHelpers) => (
+                    <div>
+                      {values.species && values.species.length > 0 ? (
+                        values.species.map((species, index) => (
+                          <div key={index} className="input-group mb-3">
+                            <Field
+                              name={`species.${index}.name`}
+                              className="form-control"
+                              placeholder="e.g neema"
+                              required
+                            />
+                            <button
+                              className="btn btn-outline-danger"
+                              type="button"
+                              onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                            >
+                              -
                             </button>
-                            )}
-                            <div>
-
-                            <div className="d-flex justify-content-between">
-                                <button type="submit" className={data ? "btn btn-primary":"btn btn-primary disabled"}>Submit Data</button>
-                                <button type="button" className="btn btn-outline-danger" onClick={logOut}>Logout</button>
-                            </div>
-                            {/* <button type="submit">Submit</button> */}
-                            </div>
+                            <button
+                              className="btn btn-outline-success"
+                              type="button"
+                              onClick={() => arrayHelpers.insert(index, "")} // insert an empty string at a position
+                            >
+                              +
+                            </button>
+                          </div>
+                        ))
+                      ) : (
+                        <button
+                          className="btn btn-outline-secondary mb-3"
+                          type="button"
+                          onClick={() => arrayHelpers.push("")}
+                        >
+                          {/* show this when user has removed all friends from the list */}
+                          Add a species
+                        </button>
+                      )}
+                      <div>
+                        <div className="d-flex justify-content-between">
+                          <button
+                            type="submit"
+                            className={
+                              token
+                                ? "btn btn-primary"
+                                : "btn btn-primary disabled"
+                            }
+                          >
+                            Submit Data
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-outline-danger"
+                            onClick={logOut}
+                          >
+                            Logout
+                          </button>
                         </div>
-                        )}
-                    />
-                    </Form>
-                )}
+                        {/* <button type="submit">Submit</button> */}
+                      </div>
+                    </div>
+                  )}
                 />
-
+              </Form>
+            )}
+          />
         </div>
-
-        </div>
-
-           
-                
-               
-           
-
-        </>
-    )
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,108 +1,116 @@
-import React, { useState,useEffect } from 'react';
-import './login.css';
-import Header from '../header/header'
-import { Link,useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./login.css";
+import Header from "../header/header";
+import { Link, useHistory } from "react-router-dom";
 
 // initial Value for the form
 
 const initialValues = Object.freeze({
-    email: "",
-    password: "",
-  });
+  email: "",
+  password: "",
+});
 
 const Login = () => {
+  let history = useHistory();
 
-    let history = useHistory();
- 
-    const [values, setValues] = useState(initialValues);
+  const [values, setValues] = useState(initialValues);
 
-    const handleInputChange = (e) => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-        const { name, value } = e.target;
-    
-        setValues({
-          ...values,
-          [name]: value,
-        });
-      };
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
-    //   const fetchData = () => {
-    //       fetch('http://localhost:5000/api/v1/users/login',{
-    //         method: 'POST',
-    //         headers: {
-    //           'Accept': 'application/json',
-    //           'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(values)
-    //       })
-    //       .then(res => res.json())
-    //       .then(data => console.log(data))
-    //   }
+  //   const fetchData = () => {
+  //       fetch('http://localhost:5000/api/v1/users/login',{
+  //         method: 'POST',
+  //         headers: {
+  //           'Accept': 'application/json',
+  //           'Content-Type': 'application/json'
+  //         },
+  //         body: JSON.stringify(values)
+  //       })
+  //       .then(res => res.json())
+  //       .then(data => console.log(data))
+  //   }
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch('https://sitematching.herokuapp.com/api/v1/users/login',{
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values)
-          })
-          .then(response => {
-            if(!response.ok){
-                throw new Error(response.status);
-            }else{
-                history.push('/home');
-                return response.json();
-            } 
-            })
-          .then(data => localStorage.setItem('user',JSON.stringify(data.token)))
-          .catch(err => console.log(err))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://sitematching.herokuapp.com/api/v1/users/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        } else {
+          history.push("/home");
+          return response.json();
+        }
+      })
+      .then((data) => localStorage.setItem("token", JSON.stringify(data.token)))
+      .catch((err) => console.log(err));
+  };
 
+  return (
+    <React.Fragment>
+      {/* Image and text  */}
 
-    return(
-        <React.Fragment>
-            {/* Image and text  */}
- 
-            <Header />
+      <Header />
 
-            {/* Form Card */}
-            <div className="card mt-5 mx-auto">
+      {/* Form Card */}
+      <div className="card mt-5 mx-auto">
+        <div className="card-body">
+          <div>
+            <h1 className="text-center m-4">LOGIN</h1>
+          </div>
 
-                <div className="card-body">
-
-                <div>
-                    <h1 className="text-center m-4">LOGIN</h1>
-                </div>
-
-                <form onSubmit={handleSubmit}>
-
-                    <div className="mb-3 ">
-                        
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" name="email" id="email" value={values.email} onChange={handleInputChange} required/>
-
-                    </div>
-
-                    <div className="mb-3 ">
-
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" name="password" id="password" value={values.password} onChange={handleInputChange} required/>
-
-                    </div>
-
-                    <button type="submit" className="btn btn-primary">Login</button>
-
-                </form>
-                
-                </div>
-
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3 ">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="email"
+                value={values.email}
+                onChange={handleInputChange}
+                required
+              />
             </div>
 
-        </React.Fragment>
-    )
-}
+            <div className="mb-3 ">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+                value={values.password}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Login;
